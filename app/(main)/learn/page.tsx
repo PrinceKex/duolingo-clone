cfa9d77d514eb716820f64827ba9872db298ef00
PrinceKex <ehigiator.ukeke@gmail.com>
2024-08-19 23:12:04 +0100

    header, stickywrapper and feedwrapper components created for the learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,16 @@
+import { FeedWrapper } from '@/components/feed-wrapper'
+import { StickyWrapper } from '@/components/sticky-wrapper'
+import { Header } from './header'
+
 const LearnPage = () => {
-  return <div>Learn Page</div>
+  return (
+    <div className='flex flex-row-reverse gap-[48px] px-6'>
+      <StickyWrapper>My Sticky Sidebar</StickyWrapper>
+      <FeedWrapper>
+        <Header title='Spanish' />
+      </FeedWrapper>
+    </div>
+  )
 }
 
 export default LearnPage
