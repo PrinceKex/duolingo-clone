07120315e487e8c5983945fa87e5ac365d6bfbf5
PrinceKex <ehigiator.ukeke@gmail.com>
2024-08-22 03:19:17 +0100

    Question bubble function added

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -3,6 +3,7 @@
 import { challengeOptions, challenges } from '@/db/schema'
 import { useState } from 'react'
 import { Header } from './header'
+import QuestionBubble from './question-bubble'
 
 type Props = {
   initialPerc: number
@@ -24,6 +25,19 @@ export const Quiz = ({
 }: Props) => {
   const [hearts, setHearts] = useState(initialHearts)
   const [percentage, setPercentage] = useState(initialPerc)
+  const [challenges] = useState(initialChallenges)
+  const [activeIndex, setActiveIndex] = useState(() => {
+    const uncompletedIndex = challenges.findIndex(
+      (challenge) => !challenge.completed
+    )
+    return uncompletedIndex === -1 ? 0 : uncompletedIndex
+  })
+
+  const challenge = challenges[activeIndex]
+  const title =
+    challenge.type === 'ASSIST'
+      ? 'Select the correct meaning'
+      : challenge.question
   return (
     <>
       <Header
@@ -31,6 +45,20 @@ export const Quiz = ({
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
       />
+      <div className='flex-1'>
+        <div className='h-full flex items-center justify-center'>
+          <div className='lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12'>
+            <h1 className='text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700'>
+              {title}
+            </h1>
+            <div>
+              {challenge.type === 'ASSIST' && (
+                <QuestionBubble question={challenge.question} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
